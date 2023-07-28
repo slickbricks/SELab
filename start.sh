c58401ee3052058c1ee58482bcdfd7f1ce712536
avianllc <93828723+avianinc@users.noreply.github.com>
2023-07-28 12:20:20 -0400

    minor work but failed

--- start.sh
+++ start.sh
@@ -72,7 +72,7 @@ start_docker() {
     fi
     
     echo "Starting docker-compose..." | tee -a $LOGFILE
-    docker image rm -f selab-tljh
+    # docker image rm -f selab-tljh
     docker-compose up -d --build
     check_status "docker-compose start"
 }
@@ -91,45 +91,45 @@ install_tljh() {
 update_base_env() {
     echo "Installing base env packages..." | tee -a $LOGFILE
     docker-compose exec tljh bash -c "set -e; \
-        sudo -E /opt/tljh/user/bin/mamba update conda -y && \
-        sudo -E /opt/tljh/user/bin/mamba env update -n base -f /tmp/updates/base_env.yaml"
+        sudo -E /opt/tljh/user/bin/mamba env update -n base -f /tmp/updates/base_env.yaml && \
+        sudo /opt/tljh/user/bin/mamba clean --all -y"
     check_status "Base environments update"
 }
 
 # This function loops through all yaml files in the /tmp/envs and builds 
 # the conda environments and the jupyter kernel.
 build_env_kernels() {
-    echo "Building environment kernels..." | tee -a $LOGFILE
-    docker-compose exec tljh bash -c 'set -e; 
-        for env_file in $(ls /tmp/envs/*.yaml); do
-            env_name=$(basename $env_file .yaml);
-            echo "Processing environment: $env_name";
-            echo "List of environments:";
-            sudo -E /opt/tljh/user/bin/mamba info --envs;
-            if [[ $(sudo -E /opt/tljh/user/bin/mamba info --envs | grep -w $env_name) ]]; then
-                echo "Updating environment $env_name";
-                sudo -E /opt/tljh/user/bin/mamba env update --name $env_name -f $env_file
-            else
-                echo "Creating environment $env_name";
-                sudo -E /opt/tljh/user/bin/mamba env create -f $env_file
-            fi
-        done && sudo -E /opt/tljh/user/bin/mamba env update -f /tmp/updates/update_kernels.yaml'
+    # echo "Building environment kernels..." | tee -a $LOGFILE
+    # docker-compose exec tljh bash -c 'set -e; 
+    #     for env_file in $(ls /tmp/envs/*.yaml); do
+    #         env_name=$(basename $env_file .yaml);
+    #         echo "Processing environment: $env_name";
+    #         echo "List of environments:";
+    #         sudo -E /opt/tljh/user/bin/mamba info --envs;
+    #         if [[ $(sudo -E /opt/tljh/user/bin/mamba info --envs | grep -w $env_name) ]]; then
+    #             echo "Updating environment $env_name";
+    #             sudo -E /opt/tljh/user/bin/mamba env update --name $env_name -f $env_file
+    #         else
+    #             echo "Creating environment $env_name";
+    #             sudo -E /opt/tljh/user/bin/mamba env create -f $env_file
+    #         fi
+    #     done && sudo -E /opt/tljh/user/bin/mamba env update -f /tmp/updates/update_kernels.yaml'
     check_status "Build environment kernels"
 }
 
 # Install elyra pipeline engine
 install_elyra() {
-    echo "Installing Elyra..." | tee -a $LOGFILE
-    docker-compose exec tljh bash -c "set -e; \
-        sudo -E /opt/tljh/user/bin/pip install --upgrade -r /tmp/envs/elyra.txt"
+    # echo "Installing Elyra..." | tee -a $LOGFILE
+    # docker-compose exec tljh bash -c "set -e; \
+    #     sudo -E /opt/tljh/user/bin/pip install --upgrade -r /tmp/envs/elyra.txt"
     check_status "Elyra Installation"
 }
 
 # Update sysmlv2 kernel model publish location
 update_sysmlv2() {
-    echo "Updating the Sysmlv2 kernel model publishing location" | tee -a $LOGFILE
-    docker-compose exec tljh bash -c "set -e; \
-        sudo sed -i 's|\"ISYSML_API_BASE_PATH\": \"http://sysml2.intercax.com:9000\"|\"ISYSML_API_BASE_PATH\": \"http://sysmlapiserver:9000\"|g' /opt/tljh/user/envs/sysmlv2/share/jupyter/kernels/sysml/kernel.json"
+    # echo "Updating the Sysmlv2 kernel model publishing location" | tee -a $LOGFILE
+    # docker-compose exec tljh bash -c "set -e; \
+    #     sudo sed -i 's|\"ISYSML_API_BASE_PATH\": \"http://sysml2.intercax.com:9000\"|\"ISYSML_API_BASE_PATH\": \"http://sysmlapiserver:9000\"|g' /opt/tljh/user/envs/sysmlv2/share/jupyter/kernels/sysml/kernel.json"
     check_status "Sysmlv2 model publish location"
 }
 
