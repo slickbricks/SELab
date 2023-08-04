fee319eb94cea4c1ba8bbfe9d7a727702577a731
avianllc <93828723+avianinc@users.noreply.github.com>
2023-08-04 07:23:58 -0400

    removed --user-requirments-txt-url

--- start.sh
+++ start.sh
@@ -80,46 +80,15 @@ install_tljh() {
     docker-compose exec tljh bash -c \
         "rm -rf /etc/skel/scratch/scratch && \
         curl -L https://tljh.jupyter.org/bootstrap.py \
-        | sudo python3 - --show-progress-page --admin $AUTH_ADMIN --plugin git+https://github.com/kafonek/tljh-shared-directory \
-        --user-requirements-txt-url https://raw.githubusercontent.com/avianinc/SELab/test/move_to_main/envs/requirements.txt \
+        | sudo python3 - --show-progress-page --admin $AUTH_ADMIN \
         --plugin git+https://github.com/plasmabio/tljh-repo2docker@master"
     check_status "Installed tljh"
-}
-
-# This function loops through all yaml files in the /tmp/envs and builds 
-# the conda environments and the jupyter kernel.
-build_env_kernels() {
-    echo "Building environment kernels..." | tee -a $LOGFILE
-    docker-compose exec tljh bash -c 'set -e; 
-        for env_file in $(ls /tmp/envs/*.yaml); do
-            env_name=$(basename $env_file .yaml);
-            echo "Processing environment: $env_name";
-            echo "List of environments:";
-            sudo -E /opt/tljh/user/bin/mamba info --envs;
-            if [[ $(sudo -E /opt/tljh/user/bin/mamba info --envs | grep -w $env_name) ]]; then
-                echo "Updating environment $env_name";
-                sudo -E /opt/tljh/user/bin/mamba env update --name $env_name -f $env_file
-            else
-                echo "Creating environment $env_name";
-                sudo -E /opt/tljh/user/bin/mamba env create -f $env_file
-            fi
-        done && sudo -E /opt/tljh/user/bin/mamba env update -f /tmp/updates/update_kernels.yaml'
-    check_status "Build environment kernels"
-}
-
-# Update sysmlv2 kernel model publish location
-update_sysmlv2_kernel() {
-    echo "Updating the Sysmlv2 kernel model publishing location" | tee -a $LOGFILE
-    docker-compose exec tljh bash -c "set -e; \
-        sudo sed -i 's|\"ISYSML_API_BASE_PATH\": \"http://sysml2.intercax.com:9000\"|\"ISYSML_API_BASE_PATH\": \"http://sysmlapiserver:9000\"|g' /opt/tljh/user/envs/sysmlv2/share/jupyter/kernels/sysml/kernel.json"
-    check_status "Sysmlv2 model publish location"
 }
 
 # Call the functions
 start_docker
 install_tljh
-build_env_kernels
-update_sysmlv2_kernel
+
 
 # Done!!!
 echo "Script completed successfully." | tee -a $LOGFILE
