e12130956bfd8a4398314e6cfc1ecced606d4ca1
avianllc <93828723+avianinc@users.noreply.github.com>
2023-08-06 15:21:25 -0400

    testing more advanced base env

--- start.sh
+++ start.sh
@@ -86,7 +86,7 @@ install_tljh() {
         "rm -rf /etc/skel/scratch/scratch && \
         curl -L https://tljh.jupyter.org/bootstrap.py \
         | sudo python3 - --show-progress-page --admin $AUTH_ADMIN --plugin git+https://github.com/kafonek/tljh-shared-directory \
-        --user-requirements-txt-url https://raw.githubusercontent.com/avianinc/SELab/test/move_to_main/envs/requirements.txt"
+        --user-requirements-txt-url https://raw.githubusercontent.com/avianinc/SELab/main/envs/requirements.txt"
     check_status "Installed tljh"
 }
 
